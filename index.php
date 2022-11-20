67b97b910d63ba164ffec67a47e2ce6c5b5209b4
Wellinton14 <99459961+Wellinton14@users.noreply.github.com>
2022-11-20 10:37:48 -0500

    App Web

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel= "stylesheet" type="text/css" href="index.css">
+    <title>Wellinton Peralta </title>
+
+</head>
+
+<body>
+
+    <header>
+        <div class= "Encabezado">
+            <h1 class="Titulo">
+                Mi Página Web !
+                
+            </h1>
+            
+            
+        </div>
+        
+    </header>
+
+    <h1 class="Titulo"> Wellinton Peralta</h1>
+
+    <div class="Contenido">
+        <p>
+            Wellinton Peralta nació el 14 de Septiembre del 2000, en la ciudad de ambato,
+            actualmente tiene 22 años, sus estudios de la primaria los realizó en la escuela Pedro Carbo,
+            sus estudios de la secundaria los realizó en la Unidad Educativa Gral. Eloy Alfaro Delgado
+            y actualmente se encuentra cursando la carrera de Ingeniería de Software en la Escuela Politécnica Nacional 
+            en la ciudad de Quito.
+
+        </p> 
+       
+
+</body>
+
+<section class = "Foto">
+        <img src= "img/WP.jpg" alt="Wellinton Peralta">
+</section>
+<h5 class="fecha" > Noviembre 2022</h5>
+
+</html>
